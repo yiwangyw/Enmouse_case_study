ac550726395d95cae84b3004dca595423a269989
yiwangyw <yiwangyww@gmail.com>
2024-12-27 01:12:31 +0900

    resnet32

--- resnet_18.py
+++ resnet_18.py
@@ -34,9 +34,9 @@ class BasicBlock1D(nn.Module):
         return out
 
 
-class ResNet1D(nn.Module):
+class ResNet181D(nn.Module):
     def __init__(self, block, layers, num_classes=10, dropout_rate=0.5):
-        super(ResNet1D, self).__init__()
+        super(ResNet181D, self).__init__()
         self.in_channels = 64
         self.conv1 = nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm1d(64)
@@ -89,4 +89,68 @@ class ResNet1D(nn.Module):
 
 
 def resnet18_1d(num_classes=2, dropout_rate=0.5):
-    return ResNet1D(BasicBlock1D, [2, 2, 2, 2], num_classes=num_classes, dropout_rate=dropout_rate)
+    return ResNet181D(BasicBlock1D, [2, 2, 2, 2], num_classes=num_classes, dropout_rate=dropout_rate)
+
+
+class ResNet321D(nn.Module):
+    def __init__(self, block, layers, num_classes=10, dropout_rate=0.5):
+        super(ResNet321D, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm1d(64)  
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1, dropout_rate=dropout_rate)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dropout_rate=dropout_rate)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dropout_rate=dropout_rate)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dropout_rate=dropout_rate)
+        
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1, dropout_rate=0.5):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv1d(self.in_channels, out_channels * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm1d(out_channels * block.expansion)
+            )
+        
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample, dropout_rate))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels, dropout_rate=dropout_rate))
+        
+        return nn.Sequential(*layers)  
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        
+        return x
+
+
+def resnet32_1d(num_classes=10, dropout_rate=0.5):
+    return ResNet301D(BasicBlock1D, [5, 5, 5, 5], num_classes=num_classes, dropout_rate=dropout_rate)
+
+
+# Example usage
+if __name__ == '__main__':
+    model = resnet32_1d(num_classes=10)
+    x = torch.randn(8, 1, 224)  # Batch size 8, 1 channel, 224 length
+    output = model(x)
+    print(output.shape)  # Should output torch.Size([8, 10])
